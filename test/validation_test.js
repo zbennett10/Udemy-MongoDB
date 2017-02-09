3af975460a55ff58d66c74eb64d73c6cfdd8b6a9
Zachary Bennett <zbennett10@hotmail.com>
2017-02-09 13:37:45 -0500

    added validation tests

---
+++ test/validation_test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+    //causing error to be thrown and making sure error message matches
+    it('requires a user name', () => {
+        const user = new User({
+            name: undefined
+        });
+        const validationResult = user.validateSync();
+        const message = validationResult.errors.name.message;
+        assert(message === 'Please provide a name.');
+    });
+
+    //causing error to be thrown and making sure error message matches
+    it('requires a user name longer than 2 characters', () => {
+        const user = new User({
+            name: 'Al'
+        });
+        const validationResult = user.validateSync();
+        const message = validationResult.errors.name.message;
+        assert(message === 'Name must be longer than 2 characters.');
+    });
+});
